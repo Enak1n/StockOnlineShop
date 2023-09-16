5927d2f1550ace85d2c42751a865d9d74facf6ce
Enak1n <masj963@gmail.com>
2023-09-17 00:48:08 +0700

    tsx

--- Frontend/stock/src/App.tsx
+++ Frontend/stock/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './app/components/layout/Header/Header'
 
 function App() {
-
+	return (
+    <Header></Header>
+	)
 }
 
-export default App;
+export default App
